ba33e4192be4010defd792175874cbc6eba4e63f
Jerod Gawne <jerodgawne@gmail.com>
2019-05-17 15:14:37 -0400

    Update setup.py
    increment version;
    update license;
    improve documentation

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
-"""LibSEA Phishlabs Setup: Jerod Gawne, 2019.03.20 <https://github.com/jerodg/>"""
+"""LibSEA Phishlabs: Setup
+   Jerod Gawne, 2019.03.20 <https://github.com/jerodg/>"""
 import logging
 import sys
 import traceback
@@ -10,6 +11,10 @@ logger = logging.getLogger(__name__)
 name = 'libsea_phishlabs'
 
 
+# todo: create documentation
+# todo: create tests
+# todo: rewrite readme in markdown
+
 def readme() -> str:
     with open('README.adoc') as f:
         return f.read()
@@ -18,36 +23,37 @@ def readme() -> str:
 if __name__ == '__main__':
     try:
         setuptools.setup(name='libsea_phishlabs',
-                         version='1.0a0.dev0',
+                         version='1.0b0.dev0',
                          description='Phishlabs API Client Library',
                          long_description=readme(),
                          long_description_content_type='text/markdown',
-                         classifiers=[
-                             'Development Status :: 3 - Alpha',
+                         classifiers=['Development Status :: 4 - Beta',
                              'Environment :: Console',
                              'Intended Audience :: End Users/Desktop',
                              'Intended Audience :: Developers',
                              'Intended Audience :: System Administrators',
-                             'License :: OSI Approved :: GNU Affero General Public License v3',
+                                      'License :: Other/Proprietary License',
                              'Natural Language :: English',
                              'Operating System :: MacOS :: MacOS X',
                              'Operating System :: Microsoft :: Windows',
                              'Operating System :: POSIX',
                              'Programming Language :: Python',
                              'Topic :: Utilities'],
-                         keywords='api client base',
-                         url='https://github.info53.com/Fifth-Third/SEA_LibSEA_PhishLabs',
+                         keywords='api client phishlabs',
+                         url='https://github.info53.com/Fifth-Third/SEA-LibSEA_PhishLabs',
                          author='Jerod Gawne',
                          author_email='jerodgawne@gmail.com',
-                         license='None/Proprietary',
+                         license='Other/Proprietary',
                          packages=setuptools.find_packages(),
-                         install_requires=[],
+                         install_requires=['aiohttp', 'aiodns', 'cchardet', 'libsea_base', 'tenacity'],
                          include_package_data=True,
-                         zip_safe=False,
+                         zip_safe=True,
                          setup_requires=['pytest-runner'],
-                         tests_require=['pytest'],
+                         tests_require=['pytest', 'pytest-cov', 'pytest-asyncio'],
                          scripts=[],
                          entry_points={'console_scripts': []},
-                         python_requires='~=3.7')
+                         python_requires='~=3.7',
+                         project_urls={
+                             'Documentation': 'https://github.info53.com/Fifth-Third/SEA-LibSEA_Phantom/tree/master/docs'})
     except Exception as excp:
         logger.exception(traceback.print_exception(*sys.exc_info()))
