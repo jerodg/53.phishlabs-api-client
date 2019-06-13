1dc5339488f935b046d54019096bcacaddd25e2f
Fox, Holly <Holly.Fox@53.com>
2019-06-13 10:39:40 -0400

    Update test_get_case_count.py

--- sea_lib_phishlabs/tests/test_get_case_count.py
+++ sea_lib_phishlabs/tests/test_get_case_count.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3.7
-"""Test Phishlabs API: Get Case Count
-   Jerod Gawne, 2019.01.09 <https://github.com/jerodg/>"""
-import logging
-import sys
+"""Phishlabs API: Test Get Case Count
+    Jerod Gawne, 2019.06.13 <https://github.com/jerodg>"""
 import time
-import traceback
-from os.path import abspath, dirname
 
 import pytest
+from sea_lib_base.base_utils import bprint
 
-from libsea_base.base_api_utils import bprint
 from sea_lib_phishlabs.phishlabs_api import PhishlabsApi
-
-logger = logging.getLogger(__name__)
-DBG = logger.isEnabledFor(logging.DEBUG)
-NFO = logger.isEnabledFor(logging.INFO)
-ROOT = dirname(abspath(__file__))
 
 
 @pytest.mark.asyncio
@@ -23,7 +14,7 @@ async def test_get_case_count():
     ts = time.perf_counter()
     bprint('Test: Get Case Count')
 
-    with PhishlabsApi(root=ROOT, sem=10) as plapi:
+    with PhishlabsApi(sem=10) as plapi:
         results = await plapi.get_case_count()
         # print('\nresults:', results)  # debug
 
@@ -32,10 +23,3 @@ async def test_get_case_count():
         print(f'Found {results} cases.')
 
     bprint(f'-> Completed in {round((time.perf_counter() - ts) / 60, 2)} minutes.')
-
-
-if __name__ == '__main__':
-    try:
-        print(__doc__)
-    except Exception as excp:
-        logger.exception(traceback.print_exception(*sys.exc_info()))
