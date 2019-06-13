1725eedb79fef7cc40be40a42923cb661be6620f
Fox, Holly <Holly.Fox@53.com>
2019-06-13 10:38:25 -0400

    Update __init__.py

--- sea_lib_phishlabs/tests/__init__.py
+++ sea_lib_phishlabs/tests/__init__.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3.7
-"""LibSEA Base Tests Init: Jerod Gawne, 2019.03.14 <https://github.com/jerodg/>"""
-import logging
-import sys
-import traceback
+"""SEAlib Phishlabs Tests Init
+    Jerod Gawne, 2019.06.13 <https://github.com/jerodg>"""
 
-from sea_lib_phishlabs.tests import test_get_case_count
+from sea_lib_phishlabs.tests import test_get_attachment, test_get_case, test_get_case_count, test_get_case_count_filtered, \
+    test_get_cases, test_get_cases_filtered, test_get_cases_filtered1, test_get_cases_filtered2
 
-___all___ = ['test_phishlabs_api']
-
-logger = logging.getLogger(__name__)
-DBG = logger.isEnabledFor(logging.DEBUG)
-NFO = logger.isEnabledFor(logging.INFO)
-
-if __name__ == '__main__':
-    try:
-        print(__doc__)
-    except Exception as excp:
-        logger.exception(traceback.print_exception(*sys.exc_info()))
+___all___ = ['test_get_attachment',
+             'test_get_case',
+             'test_get_case_count',
+             'test_get_case_count_filtered',
+             'test_get_cases',
+             'test_get_cases_filtered',
+             'test_get_cases_filtered1',
+             'test_get_cases_filtered2']
