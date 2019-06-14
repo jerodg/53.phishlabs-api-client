22e786040aab5e94b180a7e5febcc633e79691f6
Fox, Holly <Holly.Fox@53.com>
2019-06-14 07:58:42 -0400

    Update __init__.py

--- sea_lib_phishlabs/tests/__init__.py
+++ sea_lib_phishlabs/tests/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 """SEAlib Phishlabs Tests Init
-    Jerod Gawne, 2019.06.13 <https://github.com/jerodg>"""
+    Jerod Gawne, 2019.01.09 <https://github.com/jerodg>"""
 
 from sea_lib_phishlabs.tests import test_get_attachment, test_get_case, test_get_case_count, test_get_case_count_filtered, \
     test_get_cases, test_get_cases_filtered, test_get_cases_filtered1, test_get_cases_filtered2
